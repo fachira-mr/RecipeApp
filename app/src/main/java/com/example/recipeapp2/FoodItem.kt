e7993cf10334769886ec3afc008ece3651b418b0
fachira-mr <fachirarahman196@gmail.com>
2025-01-02 13:10:56 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recipeapp2/FoodItem.kt
@@ -0,0 +1,10 @@
+package com.example.recipeapp2
+
+data class FoodItem(
+    val id: String? = null,
+    val title: String? = null,
+    val category: String? = null,
+    val description: String? = null,
+    val bahan: String? = null,
+    val cara: String? = null,
+)
